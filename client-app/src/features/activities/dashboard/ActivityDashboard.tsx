336e5f8d3dd5ef09290bf949d7420bf0a3d0e9df
theodorjurubita <theodor.jurubita@gmail.com>
2023-10-09 23:20:01 +0300

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import LoadingComponent from "../../../app/layout/LoadingComponents";
+import ActivityFilters from "./ActivityFilters";
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
@@ -24,7 +25,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <GridColumn width="6">
-        <h2>Activity Filters</h2>
+        <ActivityFilters />
       </GridColumn>
     </Grid>
   );
