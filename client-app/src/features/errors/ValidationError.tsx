30775dd2eb9f49fa90b287a1bda40c45592076fb
theodorjurubita <theodor.jurubita@gmail.com>
2023-11-07 18:34:24 +0200

    End of section 13

--- client-app/src/features/errors/ValidationError.tsx
+++ client-app/src/features/errors/ValidationError.tsx
@@ -1,7 +1,7 @@
 import { Message } from "semantic-ui-react";
 
 interface Props {
-  errors: string[];
+  errors: string[] | null;
 }
 
 export default function ValidationError({ errors }: Props) {
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,14 @@
+import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { Fragment } from "react";
+import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/RegisterForm";
 
-export default function HomePage() {
+export default observer(function HomePage() {
+  const { userStore, modalStore } = useStore();
+
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
@@ -13,11 +20,32 @@ export default function HomePage() {
           />
           Reactivities
         </Header>
-        <Header as="h2" inverted content="Welcome to Reactivities" />
-        <Button as={Link} to="/activities" size="huge" inverted>
-          Take me to activities
-        </Button>
+        {userStore.isLoggedIn ? (
+          <Fragment>
+            <Header as="h2" inverted content="Welcome to Reactivities" />
+            <Button as={Link} to="/activities" size="huge" inverted>
+              Go to Activities!
+            </Button>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Button
+              onClick={() => modalStore.openModal(<LoginForm />)}
+              size="huge"
+              inverted
+            >
+              Login!
+            </Button>
+            <Button
+              onClick={() => modalStore.openModal(<RegisterForm />)}
+              size="huge"
+              inverted
+            >
+              Register!
+            </Button>
+          </Fragment>
+        )}
       </Container>
     </Segment>
   );
-}
+});
